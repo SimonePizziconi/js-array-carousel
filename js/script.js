a02cbe9160c2e3795a65fd1f5fd6a71a5edfc4a4
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-10 15:22:25 +0200

    feat: create array and element and insert the element into the container

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Creo un array con dentro le img
+const imgList = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp",]
+
+// Prendo il contenitore
+const imgContainer = document.querySelector(".container")
+
+// creo un ciclo
+for (let i = 0; i < imgList.length; i++){
+
+    // Creo un elemento HTML
+    let imgItem = (`<div class="item">
+                        <img src="${imgList[i]}" alt="">
+                    </div>`);
+    console.log(imgItem);
+
+    // L'elemento lo inserisco nel container
+    imgContainer.innerHTML += imgItem;
+}
